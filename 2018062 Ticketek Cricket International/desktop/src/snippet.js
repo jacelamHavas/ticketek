0150e27f7f071aec46f982594f91e90f9fc9a43e
James Claridge <james@claridgeand.co>
2018-06-04 23:55:17 +1000

    finalized

--- 2018062 Ticketek Cricket International/desktop/src/snippet.js
+++ 2018062 Ticketek Cricket International/desktop/src/snippet.js
@@ -5,3 +5,12 @@
         $("#eventsPageContainerInner .match-container").children().removeClass('show').addClass('hide');
         $("#eventsPageContainerInner .match-container").find(`#${current}`).addClass('show').removeClass('hide');
     });
+    $( document ).ready(function() {
+        var deadline = 'Wed, 31 Oct 2018 00:01:00 GMT+1000';
+          initializeClock('clockdiv', deadline);
+        
+          if($('.days').text() == '0'){
+            $('.days').parent().hide()
+          }
+        });
+        
