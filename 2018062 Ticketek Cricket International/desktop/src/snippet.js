6334fc7aea644f77163b0f9c3cda91e5b98cf514
Brendon Nicholas <brendon@dotdev.com.au>
2018-06-04 20:18:24 +1000

    :bug: wrap JS in optimizely to delay loading and work in preview mode :construction:

--- 2018062 Ticketek Cricket International/desktop/src/snippet.js
+++ 2018062 Ticketek Cricket International/desktop/src/snippet.js
@@ -1,9 +1,15 @@
-$('.sectionTitle').parent().replaceWith(html);
+// Retrieve the utils library
+var utils = window["optimizely"].get('utils');
 
-$('#eventsPageContainerInner .tab-item').on('click', function (e) {
+// Wait for the footer element to appear in the DOM, then change the color
+utils.waitForElement('.sectionTitle').then(function(element) {
+  $(element).parent().replaceWith(html);
+  
+  $('#eventsPageContainerInner .tab-item').on('click', function (e) {
     $('#eventsPageContainerInner .tab-item').removeClass('active');
     $(this).addClass('active');
     var current = $(this).data('attr');
     $("#eventsPageContainerInner .match-container").children().removeClass('show').addClass('hide');
     $("#eventsPageContainerInner .match-container").find('#' + current).addClass('show').removeClass('hide');
+  });
 });
--- 2018062 Ticketek Cricket International/mobile/src/snippet.js
+++ 2018062 Ticketek Cricket International/mobile/src/snippet.js
@@ -1,14 +1,20 @@
-$('.eventModuleWrap').parent().replaceWith(html);
+// Retrieve the utils library
+var utils = window["optimizely"].get('utils');
 
-$('#eventsPageContainerInner .tab-item').on('click', function (e) {
+// Wait for the footer element to appear in the DOM, then change the color
+utils.waitForElement('.eventModuleWrap').then(function(element) {
+  $(element).parent().replaceWith(html);
+  
+  $('#eventsPageContainerInner .tab-item').on('click', function (e) {
     $('#eventsPageContainerInner #nav-page').removeClass('show').addClass('hide');
     $('#eventsPageContainerInner #match-page').removeClass('hide').addClass('show');
     var current = $(this).data('attr');
     $("#eventsPageContainerInner .match-container").children().removeClass('show').addClass('hide');
     $("#eventsPageContainerInner .match-container").find('#' + current).addClass('show').removeClass('hide');
     $("#eventsPageContainerInner").find('#region').html(current);
-});
-$('#eventsPageContainerInner #backlink').on('click', function (e) {
-    $('#eventsPageContainerInner #match-page').removeClass('show').addClass('hide');
-    $('#eventsPageContainerInner #nav-page').removeClass('hide').addClass('show');
+  });
+  $('#eventsPageContainerInner #backlink').on('click', function (e) {
+      $('#eventsPageContainerInner #match-page').removeClass('show').addClass('hide');
+      $('#eventsPageContainerInner #nav-page').removeClass('hide').addClass('show');
+  });
 });
