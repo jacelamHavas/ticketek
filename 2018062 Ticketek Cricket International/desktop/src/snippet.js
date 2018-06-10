53054beb4b9fbb7dd77b51c441fc28f82f51963d
Brendon Nicholas <brendon@dotdev.com.au>
2018-06-10 20:26:23 +1000

    :bug: JS typos + fix selector + rename deploy files to .js

--- 2018062 Ticketek Cricket International/desktop/src/snippet.js
+++ 2018062 Ticketek Cricket International/desktop/src/snippet.js
@@ -1,16 +1,24 @@
-    $('#eventsPageContainerInner .tab-item').on('click', function (e) {
-        $('#eventsPageContainerInner .tab-item').removeClass('active');
-        $(this).addClass('active');
-        var current = $(this).data('attr', current);
-        $("#eventsPageContainerInner .match-container").children().removeClass('show').addClass('hide');
-        $("#eventsPageContainerInner .match-container").find(`#${current}`).addClass('show').removeClass('hide');
-    });
-    $( document ).ready(function() {
-        var deadline = 'Wed, 31 Oct 2018 00:01:00 GMT+1000';
-          initializeClock('clockdiv', deadline);
-        
-          if($('.days').text() == '0'){
-            $('.days').parent().hide()
-          }
-        });
-        
+// Retrieve the utils library
+var utils = window["optimizely"].get('utils');
+
+// Wait for the footer element to appear in the DOM, then change the color
+utils.waitForElement('.sectionTitle').then(function (element) {
+  $(element).parent().replaceWith(html);
+
+  $('#eventsPageContainerInner .tab-item').on('click', function (e) {
+    $('#eventsPageContainerInner .tab-item').removeClass('active');
+    $(this).addClass('active');
+    var current = $(this).data('attr');
+    $("#eventsPageContainerInner .match-container").children().removeClass('show').addClass('hide');
+    $("#eventsPageContainerInner .match-container").find('#' + current).addClass('show').removeClass('hide');
+  });
+
+  $(document).ready(function () {
+    var deadline = 'Wed, 31 Oct 2018 00:01:00 GMT+1000';
+    initializeClock('clockdiv', deadline);
+
+    if ($('.days').text() == '0') {
+      $('.days').parent().hide()
+    }
+  });
+});
