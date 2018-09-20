97823c8088d73b344df4879628cd707f64e4d29a
James Claridge <james@claridgeand.co>
2018-09-20 21:21:32 +1000

    both projects

---
+++ AOpen/australianOpen/2019/experiment2/dist/events.js
@@ -0,0 +1,121 @@
+const eventCalendarList = [
+  {
+    'dayName': 'Mon',
+    'dayString': '13 Jan 2019',
+    'dayNo': 13,
+    'venues': [
+      {
+        'venue': 'RLA',
+        'venueString': 'Rod Laver Arena',
+        'venueDescription': 'The best players on the biggest stage - don\'t miss a minute of the centre court action on Rod Laver Arena, the heart of the tournament.',
+        'events': [
+          {
+            'title': '7pm',
+            'product': 'SDS',
+            'btnTitle': 'Link Title Dynamic',
+            'btnClass': 'dark-blue',
+            'btnLink': 'http://google.com'
+          },
+          {
+            'title': '7pm',
+            'product': 'SDS',
+            'btnTitle': 'Link Title Dynamic',
+            'btnClass': 'orange',
+            'btnLink': 'http://google.com'
+          },
+        ]
+      },
+      {
+        'venue': 'MCA',
+        'venueString': 'Margaret Court Arena',
+        'venueDescription': 'Margaret Court Arena Description',
+        'events': [
+          {
+            'title': '7pm',
+            'product': 'SDS',
+            'btnTitle': 'Link Title Dynamic',
+            'btnClass': 'green',
+            'btnLink': 'http://google.com'
+          },
+          {
+            'title': '7pm',
+            'product': 'SDS',
+            'btnTitle': 'Link Title Dynamic',
+            'btnClass': 'green',
+            'btnLink': 'http://google.com'
+          },
+        ]
+      },
+      {
+        'venue': 'GP',
+        'venueString': 'Ground Pass',
+        'venueDescription': 'Content',
+        'events': [
+          {
+            'title': '7pm',
+            'product': 'SDS',
+            'btnTitle': 'Link Title Dynamic',
+            'btnClass': 'green',
+            'btnLink': 'http://google.com'
+          },
+          {
+            'title': '7pm',
+            'product': 'SDS',
+            'btnTitle': 'Link Title Dynamic',
+            'btnClass': 'green',
+            'btnLink': 'http://google.com'
+          },
+        ]
+      }
+    ]
+  },
+  {
+    'dayName': 'Tue',
+    'dayString': '14 Jan 2019',
+    'dayNo': 14,
+    'venues': [
+      {
+        'venue': 'RLA',
+        'venueString': 'Rod Laver Arena',
+        'venueDescription': 'Rod Laver Arena Description',
+        'events': [
+          {
+            'title': '7pm',
+            'product': 'SDS',
+            'btnTitle': 'Link Title Dynamic',
+            'btnClass': 'green',
+            'btnLink': 'http://google.com'
+          },
+          {
+            'title': '7pm',
+            'product': 'SDS',
+            'btnTitle': 'Link Title Dynamic',
+            'btnClass': 'dark-blue',
+            'btnLink': 'http://google.com'
+          },
+        ]
+      },
+      {
+        'venue': 'MCA',
+        'venueString': 'Margaret Court Arena',
+        'venueDescription': 'Margaret Court Arena Description',
+        'events': [
+          {
+            'title': 'Day Session - 2am',
+            'product': 'SDS',
+            'btnTitle': 'Link Title Dynamic',
+            'btnClass': 'blue',
+            'btnLink': 'http://google.com'
+          },
+          {
+            'title': 'Day Session - 11am',
+            'product': 'SDS',
+            'btnTitle': 'Link Title Dynamic',
+            'btnClass': 'green',
+            'btnLink': 'http://google.com'
+          },
+        ]
+      }
+    ]
+  }
+];
