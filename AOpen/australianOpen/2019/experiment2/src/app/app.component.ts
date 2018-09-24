52e281c50e38ca3801b491dab986df4f9a4df025
James Claridge <james@claridgeand.co>
2018-09-24 21:29:44 +1000

    clean up

--- AOpen/australianOpen/2019/experiment2/src/app/app.component.ts
+++ AOpen/australianOpen/2019/experiment2/src/app/app.component.ts
@@ -4,6 +4,178 @@ import { Component, OnInit } from '@angular/core';
 // THIS IS FOR PROD
 declare const eventCalendarList: any;
 
+// // THIS IS FOR DEV
+// const eventCalendarList = [
+//   {
+//     'venue': 'MCA',
+//     'venueString': 'Margaret Court Arena',
+//     'venueDescription': 'Margaret Court Arena Description',
+//     'dateString': 'Jan something',
+//     'btnClass': 'lime',
+//     'products': [
+//       {
+//         'title': 'Multiday Session',
+//         'product': 'Single Day Session',
+//         'dateString': '12 - 12 Jan',
+//         'btnTitle': 'Link Title Dynamic',
+//         'btnClass': 'blue',
+//         'times': [
+//           {
+//             'dateString': 'Mon 15 Jan',
+//             'description': 'test',
+//             'btnString': 'Get Tickets',
+//             'btnClass': 'dark-blue',
+//             'btnLink': 'google.com'
+//           },
+//           {
+//             'dateString': 'Mon 15 Jan',
+//             'description': 'test',
+//             'btnString': 'Get Tickets',
+//             'btnClass': 'dark-blue',
+//             'btnLink': 'google.com'
+//           },
+//         ]
+//       },
+//       {
+//         'title': 'Multiday Night',
+//         'product': 'SDS',
+//         'dateString': '12 - 12 Jan',
+//         'btnTitle': 'Link Title Dynamic',
+//         'btnClass': 'dark-blue',
+//         'times': [
+//           {
+//             'dateString': 'Mon 15 Jan',
+//             'description': 'test',
+//             'btnString': 'Get Tickets',
+//             'btnClass': 'dark-blue',
+//             'btnLink': 'google.com'
+//           },
+//           {
+//             'dateString': 'Mon 15 Jan',
+//             'description': 'test',
+//             'btnString': 'Get Tickets',
+//             'btnClass': 'dark-blue',
+//             'btnLink': 'google.com'
+//           },
+//         ]
+//       },
+//     ]
+//   },
+//   {
+//     'venue': 'GP',
+//     'venueString': 'Ground Pass',
+//     'venueDescription': 'Content',
+//     'dateString': 'Jan something',
+//     'btnClass': 'blue',
+//     'products': [
+//       {
+//         'title': '7pm',
+//         'product': 'SDS',
+//         'dateString': '12 - 12 Jan',
+//         'btnTitle': 'Link Title Dynamic',
+//         'btnClass': 'lime',
+//         'times': [
+//           {
+//             'dateString': 'Mon 15 Jan',
+//             'description': 'test',
+//             'btnString': 'Get Tickets',
+//             'btnClass': 'dark-blue',
+//             'btnLink': 'google.com'
+//           },
+//           {
+//             'dateString': 'Mon 15 Jan',
+//             'description': 'test',
+//             'btnString': 'Get Tickets',
+//             'btnClass': 'dark-blue',
+//             'btnLink': 'google.com'
+//           },
+//         ]
+//       },
+//       {
+//         'title': '7pm',
+//         'product': 'SDS',
+//         'dateString': '12 - 12 Jan',
+//         'btnTitle': 'Link Title Dynamic',
+//         'btnClass': 'lime',
+//         'btnLink': 'http://google.com',
+//         'times': [
+//           {
+//             'dateString': 'Mon 15 Jan',
+//             'description': 'test',
+//             'btnString': 'Get Tickets',
+//             'btnClass': 'dark-blue',
+//             'btnLink': 'google.com'
+//           },
+//           {
+//             'dateString': 'Mon 15 Jan',
+//             'description': 'test',
+//             'btnString': 'Get Tickets',
+//             'btnClass': 'dark-blue',
+//             'btnLink': 'google.com'
+//           },
+//         ]
+//       },
+//     ]
+//   },
+//   {
+//     'venue': 'MCA',
+//     'venueString': 'Ground',
+//     'venueDescription': 'Margaret Court Arena Description',
+//     'dateString': 'Jan something',
+//     'btnClass': 'orange',
+//     'products': [
+//       {
+//         'title': '7pm',
+//         'product': 'SDS',
+//         'dateString': '12 - 12 Jan',
+//         'btnTitle': 'Link Title Dynamic',
+//         'btnClass': 'lime',
+//         'btnLink': 'http://google.com',
+//         'times': [
+//           {
+//             'dateString': 'Mon 15 Jan',
+//             'description': 'test',
+//             'btnString': 'Get Tickets',
+//             'btnClass': 'dark-blue',
+//             'btnLink': 'google.com'
+//           },
+//           {
+//             'dateString': 'Mon 15 Jan',
+//             'description': 'test',
+//             'btnString': 'Get Tickets',
+//             'btnClass': 'dark-blue',
+//             'btnLink': 'google.com'
+//           },
+//         ]
+//       },
+//       {
+//         'title': '7pm',
+//         'product': 'SDS',
+//         'dateString': '12 - 12 Jan',
+//         'btnTitle': 'Link Title Dynamic',
+//         'btnClass': 'lime',
+//         'btnLink': 'http://google.com',
+//         'times': [
+//           {
+//             'dateString': 'Mon 15 Jan',
+//             'description': 'test',
+//             'btnString': 'Get Tickets',
+//             'btnClass': 'dark-blue',
+//             'btnLink': 'google.com'
+//           },
+//           {
+//             'dateString': 'Mon 15 Jan',
+//             'description': 'test',
+//             'btnString': 'Get Tickets',
+//             'btnClass': 'dark-blue',
+//             'btnLink': 'google.com'
+//           },
+//         ]
+//       },
+//     ]
+//   },
+// ];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
