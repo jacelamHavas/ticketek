b21af6fe5431cd47eadadbe4c2b6b9fce0061631
jnestour <julien@julienlenestour.com>
2018-09-25 15:45:41 +1000

    Update events-experiment1.js

--- AOpen/events-experiment1.js
+++ AOpen/events-experiment1.js
@@ -1,69 +1,118 @@
 const eventCalendarList = [
   {
     'dayName': 'Mon',
-    'dayString': '15 Jan 2019',
-    'dayNo': 15,
+    'dayString': '14 Jan 2019',
+    'dayNo': 14,
     'venues': [
       {
         'venue': 'RLA',
-        'venueString': 'Rod Laver Arena',
+        'venueString': 'ROD LAVER ARENA',
         'venueDescription': 'The best players on the biggest stage - don\'t miss a minute of the centre court action on Rod Laver Arena, the heart of the tournament.',
         'events': [
           {
-            'title': '7pm',
+            'title': 'Day Session - 11am',
             'product': 'SDS',
-            'btnTitle': 'Link Title Dynamic',
+            'btnTitle': 'Get Tickets',
             'btnClass': 'dark-blue',
-            'btnLink': 'http://google.com'
+            'btnLink': '//ausopen.ticketek.com.au/events/AOPENRLA19/venues/RLA/performances/EMAO2019614/tickets'
           },
           {
-            'title': '7pm',
+            'title': 'Night Session - 7pm',
             'product': 'SDS',
-            'btnTitle': 'Link Title Dynamic',
+            'btnTitle': 'Get Tickets',
+            'btnClass': 'dark-blue',
+            'btnLink': '//ausopen.ticketek.com.au/events/AOPENRLA19/venues/RLA/performances/EMAO2019614N/tickets'
+          },
+          {
+            'title': '1st Week Day Sessions Mon 14 Jan to Thu 17 Jan',
+            'product': 'SDS',
+            'btnTitle': 'Get Package',
             'btnClass': 'orange',
-            'btnLink': 'http://google.com'
+            'btnLink': '//ausopen.ticketek.com.au/seriesevents/EMAO0000019PA/tickets'
+          },
+          {
+            'title': '1st Week Night Sessions Mon 14 Jan to Thu 17 Jan',
+            'product': 'SDS',
+            'btnTitle': 'Get Package',
+            'btnClass': 'orange',
+            'btnLink': '//ausopen.ticketek.com.au/seriesevents/EMAO0000019PB/tickets'
           },
         ]
       },
       {
         'venue': 'MCA',
-        'venueString': 'Margaret Court Arena',
-        'venueDescription': 'Margaret Court Arena Description',
+        'venueString': 'MARGARET COURT ARENA',
+        'venueDescription': 'Margaret Court Arena will showcase world-class tennis over the first eight days and six nights of the tournament, in a state-of-the-art setting.',
         'events': [
-          {
-            'title': '7pm',
+         {
+            'title': 'Day Session - 11am',
             'product': 'SDS',
-            'btnTitle': 'Link Title Dynamic',
-            'btnClass': 'green',
-            'btnLink': 'http://google.com'
+            'btnTitle': 'Get Tickets',
+            'btnClass': 'dark-blue',
+            'btnLink': '//ausopen.ticketek.com.au/events/AOPENMCO19/venues/MCO/performances/EMAO2019614M/tickets'
           },
           {
-            'title': '7pm',
+            'title': 'Night Session - 7pm',
             'product': 'SDS',
-            'btnTitle': 'Link Title Dynamic',
-            'btnClass': 'green',
-            'btnLink': 'http://google.com'
+            'btnTitle': 'Get Tickets',
+            'btnClass': 'dark-blue',
+            'btnLink': '//ausopen.ticketek.com.au/events/AOPENMCO19/venues/MCO/performances/EMAO2019614MN/tickets'
+          },
+          {
+            'title': '1st Week Day Sessions Mon 14 Jan to Thu 17 Jan',
+            'product': 'SDS',
+            'btnTitle': 'Get Package',
+            'btnClass': 'orange',
+            'btnLink': '//ausopen.ticketek.com.au/seriesevents/EMAO0000019PK/tickets'
+          },
+          {
+            'title': '1st Week Night Sessions Mon 14 Jan to Thu 17 Jan',
+            'product': 'SDS',
+            'btnTitle': 'Get Package',
+            'btnClass': 'orange',
+            'btnLink': '//ausopen.ticketek.com.au/seriesevents/EMAO0000019PL/tickets'
+          },
+        ]
+      },
+      {
+        'venue': 'MEL',
+        'venueString': 'MELBOURNE ARENA',
+        'venueDescription': 'Soak up the buzzing atmosphere and witness all the on-court battles over the first eight days at Melbourne Arena.',
+        'events': [
+          {
+            'title': 'Full Day - 11am',
+            'product': 'SDS',
+            'btnTitle': 'Get Tickets',
+            'btnClass': 'dark-blue',
+            'btnLink': '//ausopen.ticketek.com.au/events/AOPENVFA19/venues/MELBARENA/performances/EMAO2019614V/tickets'
           },
         ]
       },
       {
         'venue': 'GP',
-        'venueString': 'Ground Pass',
-        'venueDescription': 'Content',
+        'venueString': 'GROUND PASS',
+        'venueDescription': 'Enjoy a day out at Melbourne Park with access to all outdoor courts, Melbourne Arena, AO Live Stage, AO Ballpark and much more!',
         'events': [
           {
-            'title': '7pm',
+            'title': 'Full Day - 11am',
             'product': 'SDS',
-            'btnTitle': 'Link Title Dynamic',
-            'btnClass': 'green',
-            'btnLink': 'http://google.com'
+            'btnTitle': 'Get Tickets',
+            'btnClass': 'dark-blue',
+            'btnLink': '//ausopen.ticketek.com.au/events/AOPENGP19/venues/MPK/performances/EMAO2019614GP/tickets'
           },
           {
-            'title': '7pm',
+            'title': '3 Non-Consecutive Days',
             'product': 'SDS',
-            'btnTitle': 'Link Title Dynamic',
-            'btnClass': 'green',
-            'btnLink': 'http://google.com'
+            'btnTitle': 'Get Package',
+            'btnClass': 'orange',
+            'btnLink': '//ausopen.ticketek.com.au/events/AOPEN3DP19/venues/MPK/performances/EMAO0000193D/tickets'
+          },
+          {
+            'title': '5 Non-Consecutive Days',
+            'product': 'SDS',
+            'btnTitle': 'Get Package',
+            'btnClass': 'orange',
+            'btnLink': '//ausopen.ticketek.com.au/events/AOPEN5DP19/venues/MPK/performances/EMAO0000195D/tickets'
           },
         ]
       }
@@ -71,8 +120,8 @@ const eventCalendarList = [
   },
   {
     'dayName': 'Tue',
-    'dayString': '16 Jan 2019',
-    'dayNo': 16,
+    'dayString': '15 Jan 2019',
+    'dayNo': 15,
     'venues': [
       {
         'venue': 'RLA',
