960fcd9c5be47c3d1d59cb82faea2c282afa1e90
jnestour <julien@julienlenestour.com>
2018-09-26 17:03:42 +1000

    Update events-experiment2.js

--- AOpen/events-experiment2.js
+++ AOpen/events-experiment2.js
@@ -1,6 +1,301 @@
 const bannerImage = {'url': 'https://d35kvm5iuwjt9t.cloudfront.net/dbimages/sfx207198.jpg'};
 const eventCalendarList = [
   {
+    'venue': 'RLA',
+    'venueString': 'ROD LAVER ARENA',
+    'venueDescription': 'The best players on the biggest stage - don\'t miss a minute of the centre court action on Rod Laver Arena, the heart of the tournament.',
+    'dateString': 'Jan XX',
+    'img': '//cdn.optimizely.com/img/7556701945/bad594e8a9f9459cad6f63dd4700b05c.png',
+    'btnClass': 'dark-blue',
+    'products': [
+      {
+        'title': 'Single Day Sessions',
+        'product': 'Single Day Session',
+        'dateString': '12 - 12 Jan',
+        'img': '//cdn.optimizely.com/img/7556701945/79f1c2576e3e482bba4cb4ea6a3893e8.png',
+        'btnTitle': 'Link Title Dynamic',
+        'btnClass': 'blue',
+        'times': [
+          {
+            'dateString': 'Mon 14 Jan',
+            'description': 'First Round',
+            'btnString': 'Get Tickets',
+            'btnClass': 'dark-blue',
+            'btnLink': '//ausopen.ticketek.com.au/events/AOPENRLA19/venues/RLA/performances/EMAO2019614/tickets'
+          },
+          {
+            'dateString': 'Tue 15 Jan',
+            'description': 'First Round',
+            'btnString': 'Get Tickets',
+            'btnClass': 'dark-blue',
+            'btnLink': '//ausopen.ticketek.com.au/events/AOPENRLA19/venues/RLA/performances/EMAO2019615/tickets'
+          },
+          {
+            'dateString': 'Wed 16 Jan',
+            'description': 'Second Round',
+            'btnString': 'Get Tickets',
+            'btnClass': 'dark-blue',
+            'btnLink': '//ausopen.ticketek.com.au/events/AOPENRLA19/venues/RLA/performances/EMAO2019616/tickets'
+          },
+          {
+            'dateString': 'Thu 17 Jan',
+            'description': 'Second Round',
+            'btnString': 'Get Tickets',
+            'btnClass': 'dark-blue',
+            'btnLink': '//ausopen.ticketek.com.au/events/AOPENRLA19/venues/RLA/performances/EMAO2019617/tickets'
+          },
+          {
+            'dateString': 'Fri 18 Jan',
+            'description': 'Third Round',
+            'btnString': 'Get Tickets',
+            'btnClass': 'dark-blue',
+            'btnLink': '//ausopen.ticketek.com.au/events/AOPENRLA19/venues/RLA/performances/EMAO2019618/tickets'
+          },
+          {
+            'dateString': 'Sat 19 Jan',
+            'description': 'Third Round',
+            'btnString': 'Get Tickets',
+            'btnClass': 'dark-blue',
+            'btnLink': '//ausopen.ticketek.com.au/events/AOPENRLA19/venues/RLA/performances/EMAO2019619/tickets'
+          },
+          {
+            'dateString': 'Sun 20 Jan',
+            'description': 'Fourth Round',
+            'btnString': 'Get Tickets',
+            'btnClass': 'dark-blue',
+            'btnLink': '//ausopen.ticketek.com.au/events/AOPENRLA19/venues/RLA/performances/EMAO2019620/tickets'
+          },
+          {
+            'dateString': 'Mon 21 Jan',
+            'description': 'Fourth Round',
+            'btnString': 'Get Tickets',
+            'btnClass': 'dark-blue',
+            'btnLink': '//ausopen.ticketek.com.au/events/AOPENRLA19/venues/RLA/performances/EMAO2019621/tickets'
+          },
+          {
+            'dateString': 'Tue 22 Jan',
+            'description': '',
+            'btnString': 'Get Tickets',
+            'btnClass': 'dark-blue',
+            'btnLink': '//ausopen.ticketek.com.au/events/AOPENRLA19/venues/RLA/performances/EMAO2019622/tickets'
+          },
+          {
+            'dateString': 'Wed 23 Jan',
+            'description': '',
+            'btnString': 'Get Tickets',
+            'btnClass': 'dark-blue',
+            'btnLink': '//ausopen.ticketek.com.au/events/AOPENRLA19/venues/RLA/performances/EMAO2019623/tickets'
+          },
+          {
+            'dateString': 'Thu 24 Jan',
+            'description': '',
+            'btnString': 'Get Tickets',
+            'btnClass': 'dark-blue',
+            'btnLink': '//ausopen.ticketek.com.au/events/AOPENRLA19/venues/RLA/performances/EMAO2019624/tickets'
+          },
+          {
+            'dateString': 'Fri 25 Jan',
+            'description': '',
+            'btnString': 'Get Tickets',
+            'btnClass': 'dark-blue',
+            'btnLink': '//ausopen.ticketek.com.au/events/AOPENRLA19/venues/RLA/performances/EMAO2019625T/tickets'
+          },
+          {
+            'dateString': 'Sat 26 Jan',
+            'description': '',
+            'btnString': 'Get Tickets',
+            'btnClass': 'dark-blue',
+            'btnLink': '//ausopen.ticketek.com.au/events/AOPENRLA19/venues/RLA/performances/EMAO2019626T/tickets'
+          },
+          {
+            'dateString': 'Sun 27 Jan',
+            'description': '',
+            'btnString': 'Get Tickets',
+            'btnClass': 'dark-blue',
+            'btnLink': '//ausopen.ticketek.com.au/events/AOPENRLA19/venues/RLA/performances/EMAO2019627T/tickets'
+          },
+        ]
+      },
+      {
+        'title': 'Single Night Sessions',
+        'product': 'Single Night Session',
+        'dateString': '12 - 12 Jan',
+        'img': '//cdn.optimizely.com/img/7556701945/e6ffef29a614499f948a36f89f3658b7.png',
+        'btnTitle': 'Link Title Dynamic',
+        'btnClass': 'blue',
+        'times': [
+          {
+            'dateString': 'Mon 14 Jan',
+            'description': 'First Round',
+            'btnString': 'Get Tickets',
+            'btnClass': 'dark-blue',
+            'btnLink': '//ausopen.ticketek.com.au/events/AOPENRLA19/venues/RLA/performances/EMAO2019614N/tickets'
+          },
+          {
+            'dateString': 'Tue 15 Jan',
+            'description': 'First Round',
+            'btnString': 'Get Tickets',
+            'btnClass': 'dark-blue',
+            'btnLink': '//ausopen.ticketek.com.au/events/AOPENRLA19/venues/RLA/performances/EMAO2019615N/tickets'
+          },
+          {
+            'dateString': 'Wed 16 Jan',
+            'description': 'Second Round',
+            'btnString': 'Get Tickets',
+            'btnClass': 'dark-blue',
+            'btnLink': '//ausopen.ticketek.com.au/events/AOPENRLA19/venues/RLA/performances/EMAO2019616N/tickets'
+          },
+          {
+            'dateString': 'Thu 17 Jan',
+            'description': 'Second Round',
+            'btnString': 'Get Tickets',
+            'btnClass': 'dark-blue',
+            'btnLink': '//ausopen.ticketek.com.au/events/AOPENRLA19/venues/RLA/performances/EMAO2019617N/tickets'
+          },
+          {
+            'dateString': 'Fri 18 Jan',
+            'description': 'Third Round',
+            'btnString': 'Get Tickets',
+            'btnClass': 'dark-blue',
+            'btnLink': '//ausopen.ticketek.com.au/events/AOPENRLA19/venues/RLA/performances/EMAO2019618N/tickets'
+          },
+          {
+            'dateString': 'Sat 19 Jan',
+            'description': 'Third Round',
+            'btnString': 'Get Tickets',
+            'btnClass': 'dark-blue',
+            'btnLink': '//ausopen.ticketek.com.au/events/AOPENRLA19/venues/RLA/performances/EMAO2019619N/tickets'
+          },
+          {
+            'dateString': 'Sun 20 Jan',
+            'description': 'Fourth Round',
+            'btnString': 'Get Tickets',
+            'btnClass': 'dark-blue',
+            'btnLink': '//ausopen.ticketek.com.au/events/AOPENRLA19/venues/RLA/performances/EMAO2019620N/tickets'
+          },
+          {
+            'dateString': 'Mon 21 Jan',
+            'description': 'Fourth Round',
+            'btnString': 'Get Tickets',
+            'btnClass': 'dark-blue',
+            'btnLink': '//ausopen.ticketek.com.au/events/AOPENRLA19/venues/RLA/performances/EMAO2019621N/tickets'
+          },
+          {
+            'dateString': 'Tue 22 Jan',
+            'description': '',
+            'btnString': 'Get Tickets',
+            'btnClass': 'dark-blue',
+            'btnLink': '//ausopen.ticketek.com.au/events/AOPENRLA19/venues/RLA/performances/EMAO2019622N/tickets'
+          },
+          {
+            'dateString': 'Wed 23 Jan',
+            'description': '',
+            'btnString': 'Get Tickets',
+            'btnClass': 'dark-blue',
+            'btnLink': '//ausopen.ticketek.com.au/events/AOPENRLA19/venues/RLA/performances/EMAO2019623N/tickets'
+          },
+          {
+            'dateString': 'Thu 24 Jan',
+            'description': '',
+            'btnString': 'Get Tickets',
+            'btnClass': 'dark-blue',
+            'btnLink': '//ausopen.ticketek.com.au/events/AOPENRLA19/venues/RLA/performances/EMAO2019624N/tickets'
+          },
+          {
+            'dateString': 'Fri 25 Jan',
+            'description': '',
+            'btnString': 'Get Tickets',
+            'btnClass': 'dark-blue',
+            'btnLink': '//ausopen.ticketek.com.au/events/AOPENRLA19/venues/RLA/performances/EMAO2019625T/tickets'
+          },
+          {
+            'dateString': 'Sat 26 Jan',
+            'description': '',
+            'btnString': 'Get Tickets',
+            'btnClass': 'dark-blue',
+            'btnLink': '//ausopen.ticketek.com.au/events/AOPENRLA19/venues/RLA/performances/EMAO2019626T/tickets'
+          },
+          {
+            'dateString': 'Sun 27 Jan',
+            'description': '',
+            'btnString': 'Get Tickets',
+            'btnClass': 'dark-blue',
+            'btnLink': '//ausopen.ticketek.com.au/events/AOPENRLA19/venues/RLA/performances/EMAO2019627T/tickets'
+          },
+        ]
+      },
+      {
+        'title': 'Multiday Night',
+        'product': 'SDS',
+        'dateString': '12 - 12 Jan',
+        'btnTitle': 'Link Title Dynamic',
+        'btnClass': 'dark-blue',
+        'times': [
+          {
+            'dateString': 'Mon 15 Jan',
+            'description': 'test',
+            'btnString': 'Get Tickets',
+            'btnClass': 'dark-blue',
+            'btnLink': 'google.com'
+          },
+          {
+            'dateString': 'Mon 15 Jan',
+            'description': 'test',
+            'btnString': 'Get Tickets',
+            'btnClass': 'dark-blue',
+            'btnLink': 'google.com'
+          },
+           {
+        'title': 'Single Day Sessions',
+        'product': 'Single Day Session',
+        'dateString': '12 - 12 Jan',
+        'img': 'https://d35kvm5iuwjt9t.cloudfront.net/dbimages/sfx207198.jpg',
+        'link': 'https://d35kvm5iuwjt9t.cloudfront.net/dbimages/sfx207198.jpg',
+        'btnTitle': 'Link Title Dynamic',
+        'btnClass': 'blue',
+        'times': [
+          {
+            'dateString': 'Mon 15 Jan',
+            'description': 'test',
+            'btnString': 'Get Tickets',
+            'btnClass': 'dark-blue',
+            'btnLink': 'google.com'
+          },
+          {
+            'dateString': 'Mon 15 Jan',
+            'description': 'test',
+            'btnString': 'Get Tickets',
+            'btnClass': 'dark-blue',
+            'btnLink': 'google.com'
+          },
+        ]
+      },
+      {
+        'title': 'Multiday Night',
+        'product': 'SDS',
+        'dateString': '12 - 12 Jan',
+        'btnTitle': 'Link Title Dynamic',
+        'btnClass': 'dark-blue',
+        'times': [
+          {
+            'dateString': 'Mon 15 Jan',
+            'description': 'test',
+            'btnString': 'Get Tickets',
+            'btnClass': 'dark-blue',
+            'btnLink': 'google.com'
+          },
+          {
+            'dateString': 'Mon 15 Jan',
+            'description': 'test',
+            'btnString': 'Get Tickets',
+            'btnClass': 'dark-blue',
+            'btnLink': 'google.com'
+          },
+        ]
+      },
+    ]
+  },
+   {
     'venue': 'MCA',
     'venueString': 'Margaret Court Arena',
     'venueDescription': 'Margaret Court Arena Description',
